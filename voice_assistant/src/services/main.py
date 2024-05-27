37e6802523dd7b2e1c95b793d4d3585898176fec
krishnasharmak05 <krishnasharmak05@protonmail.com>
2024-05-28 01:02:27 +0530

    Add one line

--- voice_assistant/src/services/main.py
+++ voice_assistant/src/services/main.py
@@ -5,6 +5,7 @@ from src.helper.classes import Listener, Voice_Assistant
 def main():
     listener = Listener()
     listener.start_listening()
+    print("Testing")
 # On hotkey detection, make a voice assistant and run listener();
 
 if __name__ == "__main__":
